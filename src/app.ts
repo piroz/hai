3267806f007191d535d55e74b80a97e8dd6b00cd
Hiroshi Murakami <piroz@users.noreply.github.com>
2023-05-11 05:54:26 +0900

    use hono/aws-lambda

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
-import { handle } from './aws'
+
+import { handle } from 'hono/aws-lambda'
 
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello!'))
-
 
 export default handle(app)
