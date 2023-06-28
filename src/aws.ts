1471103a7a9b8ba8b6e6cc2432cb8ddd9eaf0834
Hiroshi Murakami <piroz@users.noreply.github.com>
2023-06-29 06:32:46 +0900

    Delete aws.ts

--- src/aws.ts
+++
@@ -1,8 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export async function handler(
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> {
-  const result = { statusCode: 200, body: 'Hello World' };
-  return result;
-}
